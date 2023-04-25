b3c9908af442e2b5b88852f46de5d4a3561e9295
helloakrur <113849010+helloakrur@users.noreply.github.com>
2023-04-25 06:25:31 +0545

    mongo connection

---
+++ backend/Server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute");
+
+const app = express();
+const PORT = process.env.PORT | 3000;
+
+app.use(express.json());
+app.use(cors());
